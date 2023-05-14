b35a5da3b9dfa3291e5532933fc7c88837f4a0c8
Sherman Burruss <sherman@innovativetruckingllc.com>
2023-05-13 22:07:42 -0700

    port bug fix

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ app.listen( 80 , function(req, res){
     console.log("Listening on port 3000.");
     })
 
-    // http.createServer(app).listen(80)
-    // https.createServer(options, app).listen(443)    
+http.createServer(app).listen(80)
+https.createServer(options, app).listen(443)
+    
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + '/public'));
 
