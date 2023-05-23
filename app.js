2c8157852aefafcb81f1315037aba3b5d6e613d6
Sherman Burruss <sherman@innovativetruckingllc.com>
2023-05-23 16:29:51 -0700

    add truck list views from Monday

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const fetch = require("node-fetch");
 const app = express();
-const http = require("http");
+const https = require("https");
 const ejs = require("ejs");
 
 // http.createServer(function (req, res) {
@@ -25,11 +26,13 @@ console.log(endDay);
 
 
 app.get("/", function(req,res){
+
     res.render('index');
 })
 
 app.get("/cleanform", function(req,res){
-    res.render('cleanform', {begDay:begDay, endDay:endDay} );
+    
+    res.render('cleanform', {begDay:begDay, endDay:endDay});
 })
 
 
@@ -47,6 +50,48 @@ function getSaturday(d) {
     diff = d.getDate() + (6-day);
     return new Date(d.setDate(diff));
 }
-  
 
+// // "\"topics\""
+// async function getTruckList(){
+//     var lists = [];
+//     var groupID = "topics";
+//     var trkGroup;
+//     var trkNum;
+//     var trkType;
+//     var trkAvail;
+//     console.log(groupID);
+
+//         var qBody= JSON.stringify({query:"query{boards(ids:3886629482){groups(ids:\"topics\"){items{id,name,column_values{text,value}}}}}"});
+
+//             var qRes = await fetch("https://api.monday.com/v2",{
+//                 method: 'post',
+//                 headers: {
+//                     'Content-Type': 'application/json',
+//                     'Authorization' : 'eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjE3OTcyNzE0OCwidWlkIjozMDM3MDczOCwiaWFkIjoiMjAyMi0wOS0wN1QyMDozMzowNS4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6MTIxMTI5OTQsInJnbiI6InVzZTEifQ.kUmLRGRbFxqYmKrTLeCCDAYSkW78P0E6chgGZQ0gKqY'
+//                 },
+//                 body:qBody
+//             })
+//             .then(res => res.json())
+//             .then(res => JSON.stringify(res, null, 2));
+//             qRes=JSON.parse(qRes);
+//             console.log(qRes.data.boards[0].groups[0].items[0].name);
+//             console.log(qRes.data);
+//             for(i=0;i<qRes.data.boards[0].groups[0].items.length;i++){
+//                 var li=[];
+//                 if(groupID=="topics"){;
+//                     trkGroup=2;
+//                 }else{
+//                     trkGroup=1;
+//                 }
+//                 trkNum=qRes.data.boards[0].groups[0].items[i].name;
+//                 trktype=qRes.data.boards[0].groups[0].items[0].column_values[2].text;
+//                 trkAvail=qRes.data.boards[0].groups[0].items[0].column_values[3].text;
+
+//                 li= {trkGroup, trkNum, trkType, trkAvail}
+//                 lists.push(li);
+//                 console.log(lists);
+//             }
+            
+//     return lists;
+// }
 
